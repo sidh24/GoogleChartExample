56781df7fc3fd12a5747855bfa2d6e55f915c622
sidh24 <sidh.shar@gmail.com>
2017-12-29 15:32:55 +0000

    Add project files.

---
+++ GoogleChartExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoogleChartExample.Startup))]
+namespace GoogleChartExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
